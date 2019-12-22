c67ab7832ce6388dd55d73e680a862e43110c131
rolinj <>
2019-12-23 07:42:28 +0800

    Remove inlcusion of spaces/blanks on the list

--- string_processor/strings.py
+++ string_processor/strings.py
@@ -11,28 +11,31 @@ def char_count(word):
 
 def list_processor(**kwargs):
     """Handles the insertion of every word to correct list. """
-
-    # First word on the file will enter this block.
-    # It will be the first member of the first list from the main list.
-    if len(kwargs['main_list']) == 0:
-        kwargs['main_list'].append(list([kwargs['cleaned_text']]))
     
-    else:
-        #This variable is for tracking whether the current word 
-        #on the loop has been already inserted to a list or not.
-        inserted = False
+    #Process the entry word only if it's not a space/blank.
+    if kwargs['cleaned_text'] != '':
 
-        for single_list in kwargs['main_list']:
-            #If a list with with correct length is present, insert the word in it.
-            #Then indicate that the word has been inserted already.
-            if len(single_list[0]) == kwargs['text_char_count']:
-                single_list.append(kwargs['cleaned_text'])
-                inserted = True
-
-        #Once looped with every list available and still nothing suits with the length,
-        #create a new list for it and append to the main list.
-        if inserted == False:
+        # First word on the file will enter this block.
+        # It will be the first member of the first list from the main list.
+        if len(kwargs['main_list']) == 0:
             kwargs['main_list'].append(list([kwargs['cleaned_text']]))
+        
+        else:
+            #This variable is for tracking whether the current word 
+            #on the loop has been already inserted to a list or not.
+            inserted = False
+
+            for single_list in kwargs['main_list']:
+                #If a list with with correct length is present, insert the word in it.
+                #Then indicate that the word has been inserted already.
+                if len(single_list[0]) == kwargs['text_char_count']:
+                    single_list.append(kwargs['cleaned_text'])
+                    inserted = True 
+
+            #Once looped with every list available and still nothing suits with the length,
+            #create a new list for it and append to the main list.
+            if inserted == False:
+                kwargs['main_list'].append(list([kwargs['cleaned_text']]))
 
 if __name__ == "__main__":
     main_list = []
